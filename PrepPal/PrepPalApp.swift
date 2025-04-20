8e2699c3c91eed54a2cca6dcfb2410ed031bb054
Abdulmujeeb Lawal <lawalmujeeb78@gmail.com>
2025-04-20 17:17:39 -0400

    removed the shared container item as that wasnt needed

--- PrepPal/PrepPalApp.swift
+++ PrepPal/PrepPalApp.swift
@@ -18,27 +18,11 @@ struct PrepPalApp: App {
         FirebaseApp.configure()
     }
     
-    var sharedModelContainer: ModelContainer = {
-        let schema = Schema([
-            Item.self,
-            User.self,
-            Meal.self,
-            Ingredient.self
-        ])
-        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
-
-        do {
-            return try ModelContainer(for: schema, configurations: [modelConfiguration])
-        } catch {
-            fatalError("Could not create ModelContainer: \(error)")
-        }
-    }()
 
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environmentObject(authModel)
         }
-        .modelContainer(sharedModelContainer)
     }
 }
